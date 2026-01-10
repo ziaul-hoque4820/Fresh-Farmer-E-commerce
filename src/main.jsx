3c5bff4e49e9b07f1763b3bc62297e1240cbe2b4
Ziaul Haque <ziaulhoque4820@gmail.com>
2026-01-10 15:41:56 +0600

    use Context API and firebase Authentication functionlity done in AuthProvider

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './router/Router.jsx'
+import AuthProvider from './context/authContext/AuthProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
+    <StrictMode>
+        <AuthProvider>
+            <RouterProvider router={router} />
+        </AuthProvider>
+    </StrictMode>,
 )
